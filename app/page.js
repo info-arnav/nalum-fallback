53b239def24980664984a7dd8d4027f0c151deb5
Arnav Gupta <info@arnavgupta.net>
2024-10-23 00:56:23 +0530

    fix: removed bugs

--- app/page.js
+++ app/page.js
@@ -31,23 +31,24 @@ export default function Home() {
           <div className="col">
             <h1>Welcome, alumni, students, and friends of NSUT!</h1>
             <p>
-              Whether you're here to reconnect, expand your professional
-              network, support NSUT’s growth, or explore the impact of giving
-              back, you’re in the right place. Our portal offers you a seamless
-              way to engage with the vibrant NSUT community, get involved with
-              alumni activities, and discover opportunities to contribute.
+              Whether you&apos;re here to reconnect, expand your professional
+              network, support NSUT&apos;s growth, or explore the impact of
+              giving back, you&apos;re in the right place. Our portal offers you
+              a seamless way to engage with the vibrant NSUT community, get
+              involved with alumni activities, and discover opportunities to
+              contribute.
             </p>
             <p>
-              For those looking to give back, we’ll guide you on how to make a
-              difference, where your support matters most, and the legacy it
-              leaves behind. If you’re here to reconnect, explore our network,
-              career services, and upcoming alumni events. Discover the NSUT
-              that inspires you.
+              For those looking to give back, we&apos;ll guide you on how to
+              make a difference, where your support matters most, and the legacy
+              it leaves behind. If you&apos;re here to reconnect, explore our
+              network, career services, and upcoming alumni events. Discover the
+              NSUT that inspires you.
             </p>
             <p>
-              Alumni, students, and friends like you fuel NSUT’s journey toward
-              greater heights. Join us and stay proud to be part of the NSUT
-              family.
+              Alumni, students, and friends like you fuel NSUT&apos;s journey
+              toward greater heights. Join us and stay proud to be part of the
+              NSUT family.
             </p>
           </div>
         </div>
--- components/notifications.js
+++ components/notifications.js
@@ -3,7 +3,7 @@ export default function Notifications({ messages }) {
     <div className="notifs">
       {messages.map((e) => {
         return (
-          <div className="notif">
+          <div className="notif" key={e.title}>
             <div className="header">{e.title}</div>
             <div className="body">{e.message}</div>
           </div>
