c35656b9366318b0785fdc9bc6cc400e9c3de997
Arnav Gupta <info@arnavgupta.net>
2024-10-23 13:32:18 +0530

    fix: footer not responsive corrected

--- components/footer.js
+++ components/footer.js
@@ -7,24 +7,26 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer>
-      <div className="row">
+      <div className="cell">
         <Link href="/">
           <Image src={NsutLogo} height={50}></Image>
         </Link>
       </div>
-      <div className="row">
-        <Link href="mailto:admin@alumninet.in">admin@alumninet.in</Link>
-      </div>
-      <div className="row">
-        <a className="icons" href="https://www.instagram.com/nalum.nsut/">
-          <Image src={InstagramIcon} height={20} width={20}></Image>
-        </a>
-        <a
-          className="icons"
-          href="https://www.linkedin.com/company/alumninet-in/"
-        >
-          <Image src={LinkedinIcon} height={30} width={30}></Image>
-        </a>
+      <div className="cell long">
+        <div className="cell">
+          <Link href="mailto:admin@alumninet.in">admin@alumninet.in</Link>
+        </div>
+        <div className="cell">
+          <a className="icons" href="https://www.instagram.com/nalum.nsut/">
+            <Image src={InstagramIcon} height={20} width={20}></Image>
+          </a>
+          <a
+            className="icons"
+            href="https://www.linkedin.com/company/alumninet-in/"
+          >
+            <Image src={LinkedinIcon} height={30} width={30}></Image>
+          </a>
+        </div>
       </div>
     </footer>
   );
